d0381cf58e30fc8fb55e672f02d2d54e5e4e3d80
Ruman67 <126407391+Ruman67@users.noreply.github.com>
2024-05-29 20:58:40 +0545

    created edit,delete button

---
+++ src/components/Todo.js
@@ -0,0 +1,33 @@
+// Todo.js
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Todo = ({ todo, toggleComplete, deleteTodo, editTodo }) => {
+  if (!todo) return null;
+
+  return (
+    <div className='Todo'>
+      <p onClick={() => toggleComplete(todo.id)} className={`${todo.completed ? 'completed' : ''}`}>
+        {todo.task}
+      </p>
+      <div>
+        <button onClick={() => editTodo(todo.id)}>Edit</button>
+        <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+      </div>
+    </div>
+  );
+};
+
+Todo.propTypes = {
+  todo: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    task: PropTypes.string.isRequired,
+    completed: PropTypes.bool.isRequired,
+    isEditing: PropTypes.bool.isRequired
+  }).isRequired,
+  toggleComplete: PropTypes.func.isRequired,
+  deleteTodo: PropTypes.func.isRequired,
+  editTodo: PropTypes.func.isRequired
+};
+
+export default Todo;
